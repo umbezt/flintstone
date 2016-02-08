5ebdf954a9df6553e92d77acf173b9ed800be560
Oscar Mwanandimai <oscar.mwana@gmail.com>
2016-02-08 16:42:02 +0200

    Rolling set up

---
+++ flintstone.php
@@ -0,0 +1,11 @@
+<?php
+
+/*
+Plugin Name: Flintstone
+Plugin URI: http://github.com/oscarmwana/flintstone
+Description: A plugin to help the Flint Studio team QA a site before launch.
+Version: 1.0
+Author: Oscar M
+Author URI: http://github.com/oscarmwana
+License: A "Slug" license name e.g. GPL2
+*/
